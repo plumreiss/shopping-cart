e8c6cb0e32cf69f349bfca074f0099bed1bc79c7
Angel Padrino <plumreiss@gmail.com>
2020-11-29 17:38:38 -0400

    feat: adding products

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
+const items = document.getElementById('items')
+const templateCard = document.getElementById('template-card').content
+const fragment = document.createDocumentFragment()
+
 document.addEventListener('DOMContentLoaded', () => {
     fetchData()
 })
@@ -6,8 +10,21 @@ const fetchData = async () => {
     try {
         const res = await fetch('api.json')
         const data = await res.json()
-        console.log(data)
+        addCards(data)
     } catch (error) {
         console.log(error)
     }
+}
+
+const addCards = data => {
+    data.forEach(product => {
+        console.log(product.title)
+        templateCard.querySelector('h5').textContent = product.title
+        templateCard.querySelector('p').textContent = product.precio
+        templateCard.querySelector('img').setAttribute('src', product.thumbnailUrl)
+        templateCard.querySelector('.btn-dark').dataset.id = product.id
+        const clone = templateCard.cloneNode(true)
+        fragment.appendChild(clone)
+    });
+    items.appendChild(fragment)
 }
