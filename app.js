4a757b845b5ce432acbfd421ca094631e9f3426f
Angel Padrino <plumreiss@gmail.com>
2020-11-29 23:32:06 -0400

    feat: add products to the cart

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const items = document.getElementById('items')
 const templateCard = document.getElementById('template-card').content
 const fragment = document.createDocumentFragment()
-
+let cart = {}
 document.addEventListener('DOMContentLoaded', () => {
     fetchData()
 })
@@ -31,4 +31,27 @@ const addCards = data => {
     });
     items.appendChild(fragment)
 }
+
+const addCart = (e) => {
+    if (e.target.classList.contains('btn-dark')) {
+        setCart(e.target.parentElement)
+    }
+    e.stopPropagation()
+}
+
+const setCart = object => {
+    console.log(object)
+    const product = {
+        id: object.querySelector('.btn-dark').dataset.id,
+        title: object.querySelector('h5').textContent,
+        precio: object.querySelector('p').textContent,
+        cantidad: 1
+    }
+    if (cart.hasOwnProperty(product.id)) {
+        product.cantidad = ++cart[product.id].cantidad
+    }
+    cart[product.id] = { ...product } //spread operator
+    console.log(cart)
+}
+
 
